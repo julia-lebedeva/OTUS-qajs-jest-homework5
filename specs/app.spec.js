7f2fc897feeb2d40fea76534fbb110c77bad8be0
Yulia Lebedeva <yulia.lebedeva@jugru.org>
2024-01-21 13:45:53 +0300

    intitial commit

--- specs/app.spec.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import { nameIsValid, fullTrim, getTotal } from "../src/app.js";
-
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
-});
