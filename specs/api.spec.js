536894b6cd4f91b4f15745f1c6c949d9c305cb2d
Yulia Lebedeva <yulia.lebedeva@jugru.org>
2024-01-27 19:21:14 +0300

    five API tests final version

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -1,9 +1,25 @@
 const { faker } = require("@faker-js/faker");
-const randomLogin = faker.word.sample();
-const randomAltLogin = faker.word.sample();
-const randomPassword = faker.internet.password( { lengh : 8, memorable: false, pattern: /^[A-Z]*$/, prefix: 'a0$'} );
-const randomAltPassword = faker.internet.password( { lengh : 8, memorable: false, pattern: /^[A-Z]*$/, prefix: 'a0$'} );
-const randomBadPassword = faker.word.sample();
+
+const createTestData = () => {
+  const testData = {
+    login: faker.word.sample(),
+    password: faker.internet.password({
+      lengh: 8,
+      memorable: false,
+      pattern: /^[A-Z]*$/,
+      prefix: "a0$",
+    }),
+    altLogin: faker.word.sample(),
+    altPassword: faker.internet.password({
+      lengh: 8,
+      memorable: false,
+      pattern: /^[A-Z]*$/,
+      prefix: "b1$",
+    }),
+    badPassword: faker.word.sample(),
+  };
+  return testData;
+};
 
 async function createUser(login, password) {
   const response = await fetch("https://bookstore.demoqa.com/Account/v1/User", {
@@ -17,7 +33,9 @@ async function createUser(login, password) {
   return response;
 }
 async function generateToken(login, password) {
-  const response = await fetch("https://bookstore.demoqa.com/Account/v1/GenerateToken", {
+  const response = await fetch(
+    "https://bookstore.demoqa.com/Account/v1/GenerateToken",
+    {
       method: "post",
       body: JSON.stringify({
         userName: login,
@@ -29,35 +47,36 @@ async function generateToken(login, password) {
   return response;
 }
 describe("5 api tests bookstore service", () => {
+  const testData = createTestData();
   test("1) Создание нового пользователя", async () => {
-    const response = await createUser(randomLogin, randomPassword);
+    const response = await createUser(testData.login, testData.password);
     expect(response.status).toBe(201);
     const data = await response.json();
-    expect(data.username).toBe(randomLogin);
+    expect(data.username).toBe(testData.login);
   });
   test("2) Создание пользователя c ошибкой - логин уже используется", async () => {
-    const response = await createUser(randomLogin, randomPassword);
+    const response = await createUser(testData.login, testData.password);
     const data = await response.json();
     expect(response.status).toBe(406);
     expect(data.code).toBe("1204");
     expect(data.message).toBe("User exists!");
   });
   test("3) Создание пользователя c ошибкой - пароль не проходит", async () => {
-    const response = await createUser(randomAltLogin, randomBadPassword);
+    const response = await createUser(testData.altLogin, testData.badPassword);
     const data = await response.json();
     expect(response.status).toBe(400);
     expect(data.code).toBe("1300");
     expect(data.message).toMatch(/Passwords must have at least/);
   });
   test("4) Генерация токена успешно", async () => {
-    const response = await generateToken(randomLogin, randomPassword);
+    const response = await generateToken(testData.login, testData.password);
     const data = await response.json();
     expect(response.status).toBe(200);
     expect(data.status).toBe("Success");
     expect(data.result).toBe("User authorized successfully.");
   });
   test("5) Гененрация c ошибкой", async () => {
-    const response = await generateToken(randomLogin, randomAltPassword);
+    const response = await generateToken(testData.login, testData.altPassword);
     const data = await response.json();
     expect(response.status).toBe(200);
     expect(data.status).toBe("Failed");
